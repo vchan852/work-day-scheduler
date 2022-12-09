000fdef6ed23f84018f813fa6a1c4f4e582d8fd1
Vanessa Chan <vchan@Vanessas-MacBook-Air.local>
2022-12-09 00:08:47 -0800

    adding css styles

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
   
   // CALLS FOR TIME DIFFERENT IN AUDIT TASKS FOR LATER FUNCTION
-  
+
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
